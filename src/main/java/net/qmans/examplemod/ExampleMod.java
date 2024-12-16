9a35b6c808fa24376b005d98d4bd38518b21a705
Qmans2000 <mokryak.stas.d@gmail.com>
2024-12-16 12:08:45 +0200

    test

--- src/main/java/net/qmans/examplemod/ExampleMod.java
+++ src/main/java/net/qmans/examplemod/ExampleMod.java
@@ -44,7 +44,6 @@ public class ExampleMod {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
